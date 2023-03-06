2007caec24bd592b7e405c692faed9aaa82e8b25
Luis Eduardo <107561355+lepm96@users.noreply.github.com>
2023-03-06 11:49:10 -0300

    atividade

--- Java/main.java
+++ Java/main.java
@@ -1,3 +1,20 @@
-public static void main(String[] args) {
-    
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
+        
+
+        System.out.println("Escolha a opção desejada:\n1) Soma\n2) Subtração\n3) Multiplicação\n4) Divisão");
+        
+        
+        
+
+        
+        
+
+
+
+       entrada.close();
+    }
 }
